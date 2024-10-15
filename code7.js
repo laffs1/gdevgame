6e213779c419708a18ac8f23df3a54c5122a7483
Robosapien737 <91835339+Robosapien737@users.noreply.github.com>
2024-10-14 20:42:16 -0700

    Added game

---
+++ code7.js
@@ -0,0 +1,138 @@
+gdjs.End_32ScreenCode = {};
+gdjs.End_32ScreenCode.localVariables = [];
+gdjs.End_32ScreenCode.GDterrainBlockObjects1= [];
+gdjs.End_32ScreenCode.GDterrainBlockObjects2= [];
+gdjs.End_32ScreenCode.GDcollectibleObjects1= [];
+gdjs.End_32ScreenCode.GDcollectibleObjects2= [];
+gdjs.End_32ScreenCode.GDDoorObjects1= [];
+gdjs.End_32ScreenCode.GDDoorObjects2= [];
+gdjs.End_32ScreenCode.GDhazardBlockObjects1= [];
+gdjs.End_32ScreenCode.GDhazardBlockObjects2= [];
+gdjs.End_32ScreenCode.GDSparkleParticleObjects1= [];
+gdjs.End_32ScreenCode.GDSparkleParticleObjects2= [];
+gdjs.End_32ScreenCode.GDbackgroundImageObjects1= [];
+gdjs.End_32ScreenCode.GDbackgroundImageObjects2= [];
+gdjs.End_32ScreenCode.GDDownButtonObjects1= [];
+gdjs.End_32ScreenCode.GDDownButtonObjects2= [];
+gdjs.End_32ScreenCode.GDmainPlayer2Objects1= [];
+gdjs.End_32ScreenCode.GDmainPlayer2Objects2= [];
+gdjs.End_32ScreenCode.GDTextObjects1= [];
+gdjs.End_32ScreenCode.GDTextObjects2= [];
+gdjs.End_32ScreenCode.GDterrainBlock2Objects1= [];
+gdjs.End_32ScreenCode.GDterrainBlock2Objects2= [];
+gdjs.End_32ScreenCode.GDcollectibleObjects1= [];
+gdjs.End_32ScreenCode.GDcollectibleObjects2= [];
+gdjs.End_32ScreenCode.GDmainPlayerObjects1= [];
+gdjs.End_32ScreenCode.GDmainPlayerObjects2= [];
+gdjs.End_32ScreenCode.GDterrainBlockObjects1= [];
+gdjs.End_32ScreenCode.GDterrainBlockObjects2= [];
+gdjs.End_32ScreenCode.GDDoorObjects1= [];
+gdjs.End_32ScreenCode.GDDoorObjects2= [];
+gdjs.End_32ScreenCode.GDfake_9595one_9595_9595_9595Objects1= [];
+gdjs.End_32ScreenCode.GDfake_9595one_9595_9595_9595Objects2= [];
+
+
+gdjs.End_32ScreenCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+{isConditionTrue_0 = runtimeScene.getOnceTriggers().triggerOnce(16253076);
+}
+if (isConditionTrue_0) {
+isConditionTrue_0 = false;
+{let isConditionTrue_1 = false;
+isConditionTrue_0 = false;
+{
+isConditionTrue_1 = gdjs.evtTools.input.isKeyPressed(runtimeScene, "Return");
+if(isConditionTrue_1) {
+    isConditionTrue_0 = true;
+}
+}
+{
+}
+}
+}
+if (isConditionTrue_0) {
+{gdjs.evtTools.window.openURL("https://laffs1.github.io", runtimeScene);
+}}
+
+}
+
+
+};
+
+gdjs.End_32ScreenCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.End_32ScreenCode.GDterrainBlockObjects1.length = 0;
+gdjs.End_32ScreenCode.GDterrainBlockObjects2.length = 0;
+gdjs.End_32ScreenCode.GDcollectibleObjects1.length = 0;
+gdjs.End_32ScreenCode.GDcollectibleObjects2.length = 0;
+gdjs.End_32ScreenCode.GDDoorObjects1.length = 0;
+gdjs.End_32ScreenCode.GDDoorObjects2.length = 0;
+gdjs.End_32ScreenCode.GDhazardBlockObjects1.length = 0;
+gdjs.End_32ScreenCode.GDhazardBlockObjects2.length = 0;
+gdjs.End_32ScreenCode.GDSparkleParticleObjects1.length = 0;
+gdjs.End_32ScreenCode.GDSparkleParticleObjects2.length = 0;
+gdjs.End_32ScreenCode.GDbackgroundImageObjects1.length = 0;
+gdjs.End_32ScreenCode.GDbackgroundImageObjects2.length = 0;
+gdjs.End_32ScreenCode.GDDownButtonObjects1.length = 0;
+gdjs.End_32ScreenCode.GDDownButtonObjects2.length = 0;
+gdjs.End_32ScreenCode.GDmainPlayer2Objects1.length = 0;
+gdjs.End_32ScreenCode.GDmainPlayer2Objects2.length = 0;
+gdjs.End_32ScreenCode.GDTextObjects1.length = 0;
+gdjs.End_32ScreenCode.GDTextObjects2.length = 0;
+gdjs.End_32ScreenCode.GDterrainBlock2Objects1.length = 0;
+gdjs.End_32ScreenCode.GDterrainBlock2Objects2.length = 0;
+gdjs.End_32ScreenCode.GDcollectibleObjects1.length = 0;
+gdjs.End_32ScreenCode.GDcollectibleObjects2.length = 0;
+gdjs.End_32ScreenCode.GDmainPlayerObjects1.length = 0;
+gdjs.End_32ScreenCode.GDmainPlayerObjects2.length = 0;
+gdjs.End_32ScreenCode.GDterrainBlockObjects1.length = 0;
+gdjs.End_32ScreenCode.GDterrainBlockObjects2.length = 0;
+gdjs.End_32ScreenCode.GDDoorObjects1.length = 0;
+gdjs.End_32ScreenCode.GDDoorObjects2.length = 0;
+gdjs.End_32ScreenCode.GDfake_9595one_9595_9595_9595Objects1.length = 0;
+gdjs.End_32ScreenCode.GDfake_9595one_9595_9595_9595Objects2.length = 0;
+
+gdjs.End_32ScreenCode.eventsList0(runtimeScene);
+gdjs.End_32ScreenCode.GDterrainBlockObjects1.length = 0;
+gdjs.End_32ScreenCode.GDterrainBlockObjects2.length = 0;
+gdjs.End_32ScreenCode.GDcollectibleObjects1.length = 0;
+gdjs.End_32ScreenCode.GDcollectibleObjects2.length = 0;
+gdjs.End_32ScreenCode.GDDoorObjects1.length = 0;
+gdjs.End_32ScreenCode.GDDoorObjects2.length = 0;
+gdjs.End_32ScreenCode.GDhazardBlockObjects1.length = 0;
+gdjs.End_32ScreenCode.GDhazardBlockObjects2.length = 0;
+gdjs.End_32ScreenCode.GDSparkleParticleObjects1.length = 0;
+gdjs.End_32ScreenCode.GDSparkleParticleObjects2.length = 0;
+gdjs.End_32ScreenCode.GDbackgroundImageObjects1.length = 0;
+gdjs.End_32ScreenCode.GDbackgroundImageObjects2.length = 0;
+gdjs.End_32ScreenCode.GDDownButtonObjects1.length = 0;
+gdjs.End_32ScreenCode.GDDownButtonObjects2.length = 0;
+gdjs.End_32ScreenCode.GDmainPlayer2Objects1.length = 0;
+gdjs.End_32ScreenCode.GDmainPlayer2Objects2.length = 0;
+gdjs.End_32ScreenCode.GDTextObjects1.length = 0;
+gdjs.End_32ScreenCode.GDTextObjects2.length = 0;
+gdjs.End_32ScreenCode.GDterrainBlock2Objects1.length = 0;
+gdjs.End_32ScreenCode.GDterrainBlock2Objects2.length = 0;
+gdjs.End_32ScreenCode.GDcollectibleObjects1.length = 0;
+gdjs.End_32ScreenCode.GDcollectibleObjects2.length = 0;
+gdjs.End_32ScreenCode.GDmainPlayerObjects1.length = 0;
+gdjs.End_32ScreenCode.GDmainPlayerObjects2.length = 0;
+gdjs.End_32ScreenCode.GDterrainBlockObjects1.length = 0;
+gdjs.End_32ScreenCode.GDterrainBlockObjects2.length = 0;
+gdjs.End_32ScreenCode.GDDoorObjects1.length = 0;
+gdjs.End_32ScreenCode.GDDoorObjects2.length = 0;
+gdjs.End_32ScreenCode.GDfake_9595one_9595_9595_9595Objects1.length = 0;
+gdjs.End_32ScreenCode.GDfake_9595one_9595_9595_9595Objects2.length = 0;
+
+
+return;
+
+}
+
+gdjs['End_32ScreenCode'] = gdjs.End_32ScreenCode;
